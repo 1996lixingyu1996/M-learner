f2e73c07accde3bcad2ccdef4d5de61830d506e4
Xingyu Li <46596546+1996lixingyu1996@users.noreply.github.com>
2025-05-10 11:56:23 -0500

    Update and rename Experiment5_no_mediator.R to Experiment3_no_mediator.R

--- simulations/Experiment3_no_mediator.R
+++ simulations/Experiment3_no_mediator.R
@@ -1,4 +1,4 @@
-##### Experiment 5- global ######
+##### Experiment 3- global ######
 
 ##### Indirect experiment ########
 
@@ -140,9 +140,9 @@ f_exp1 <- function(seed){
   
   
   
-  data_save_path = paste0("/Users/xli36/Downloads/R_packages/myproject/expe5_nomediator/seed_",seed,".csv")
+  data_save_path = paste0("/seed_",seed,".csv")
   rio::export(data, data_save_path)
-  save_path = paste0("/Users/xli36/Downloads/R_packages/myproject/expe5_nomediator/seed_",seed,".rds")
+  save_path = paste0("/seed_",seed,".rds")
   result[[1]] = MSE
   result[[2]] = MAE
   result[[3]] = data_flag
@@ -154,13 +154,12 @@ f_exp1 <- function(seed){
   rio::export(result, save_path)
 }
 
-#data = rio::import(paste0("/Users/xli36/Downloads/R_packages/myproject/expe1/seed_",1,".rds"))
 library(Rtsne)
 library(rio)
 library(snowfall)
 library(parallel)
 
-my_new_folder = "/Users/xli36/Downloads/R_packages/myproject/expe5_nomediator"
+my_new_folder = ""
 if (!dir.exists("my_new_folder")) {
   dir.create("my_new_folder")
 }
@@ -172,7 +171,7 @@ sfLibrary(rpart)
 sfLibrary(randomForestSRC)
 sfLibrary(base)
 sfLibrary(Rtsne)
-sfSource("/Users/xli36/Desktop/rf_test/tree_fit_part.R")
+sfSource("tree_fit_part.R")
 
 result_vector = sfLapply(1:100, f_exp1)
 
